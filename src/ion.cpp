878563b82d6ccd130a1f5fd21a7c34f691215b94
Sandeep Gupta <sandeep@vaticlabs.com>
2020-05-12 23:13:37 -0400

    Add start tokenizing

--- src/ion.cpp
+++ src/ion.cpp
@@ -1,48 +1,127 @@
 #include <cassert>
+#include <cctype>
+#include <cstdint>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <type_traits>
-#include <cstdio>
 
 template <typename T>
 struct vector {
-  static_assert(std::is_trivial_v<T> && std::is_standard_layout_v<T>);
-  ~vector() { delete[] ptr_; }
+  ~vector() { free(ptr_); }
   void push_back(const T& t) {
     if (size_ == capacity_) {
       grow();
     }
     assert(size_ < capacity_);
 
-    ptr_[size_++] = t;
-    return;
+    ptr()[size_++] = t;
   }
   int size() const { return size_; }
   int capacity() const { return capacity_; }
-  T& operator[](int idx) { return ptr_[idx]; }
+  T& operator[](int idx) { return ptr()[idx]; }
 
  private:
   void grow() {
     assert(size_ == capacity_);
     auto new_capacity = (capacity_) ? 2 * capacity_ : 1;
-    T* nptr = ::new T(new_capacity);
-    assert(nptr);
-    std::memcpy(nptr, ptr_, sizeof(T) * size_);
-    ptr_ = nptr;
+    ptr_ = (ptr_) ? realloc(ptr_, new_capacity * sizeof(T))
+                  : malloc(new_capacity * sizeof(T));
+    assert(ptr_);
     capacity_ = new_capacity;
   }
+  T* ptr() { return reinterpret_cast<T*>(ptr_); }
 
   int capacity_ = 0;
   int size_ = 0;
-  T* ptr_ = nullptr;
+  void* ptr_ = nullptr;
 };
 
-int main() {
+void vec_test() {
   vector<int> buf;
-  buf.push_back(42);
-  buf.push_back(1234);
-  for (int i = 0; i < buf.size(); ++i) {
-    printf("%d\n", buf[i]);
+  const int N = 1024;
+  for (int i = 0; i < N; ++i) {
+    buf.push_back(i);
   }
-  return 0;
+  assert(buf.size() == N);
+  for (int i = 0; i < N; ++i) {
+    assert(buf[i] == i);
+  }
+}
+
+enum TokenKind { TokenInt = 128, TokenName };
+
+struct Token {
+  TokenKind kind;
+  union {
+    uint64_t val;
+    struct {
+      const char* start;
+      const char* end;
+    };
+  };
+};
+void print_token(const Token& token) {
+  printf("kind: %d", token.kind);
+  switch (token.kind) {
+    case TokenInt:
+      printf(" value: %lu", token.val);
+      break;
+    case TokenName:
+      printf(" value: %.*s", (int)(token.end - token.start), token.start);
+      break;
+    default:
+      break;
+  }
+  puts("");
+}
+
+class Tokenizer {
+ public:
+  Tokenizer(const char* stream) : stream_(stream) {}
+  Token next() {
+    Token token{};
+    switch (*stream_) {
+      case '0' ... '9': {
+        uint64_t val = 0;
+        while (std::isdigit(*stream_)) {
+          val *= 10;
+          val += *stream_++ - '0';
+        }
+        token.kind = TokenInt;
+        token.val = val;
+      } break;
+      case 'a' ... 'z':
+      case 'A' ... 'Z':
+      case '_': {
+        token.start = stream_;
+        while (std::isalnum(*stream_) || (*stream_ == '_')) {
+          stream_++;
+        }
+        token.end = stream_;
+        token.kind = TokenName;
+      } break;
+      default:
+        token.kind = static_cast<TokenKind>(*stream_++);
+        break;
+    }
+    return token;
+  }
+
+ private:
+  const char* stream_;
+};
+
+void lex_test() {
+  const char* str = "+()_HELLO+997-hello";
+  Tokenizer tokenizer(str);
+  auto token = tokenizer.next();
+  while (token.kind) {
+    print_token(token);
+    token = tokenizer.next();
+  }
+}
+
+int main() {
+  vec_test();
+  lex_test();
 }
